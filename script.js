336c3ea0d054d0d3fbdb1e976f4adf4d9a6e48d7
Jesica Kolibski!git push origin mastergit config --global user.name Jesica <jkolibski@gmail.com>
2020-08-24 10:26:09 -0300

    ToDo final

---
+++ script.js
@@ -0,0 +1,52 @@
+const mostrarToDo = () => {
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+    .then(res => {
+        console.log(res.data)// res.data (porque la rta se guarda en una data)
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+            res.data.forEach(todo => {
+                const user = response.data.find(user => user.id === todo.userId);
+                const li = document.createElement('li');
+                const span = document.createElement('span');   
+                const botonEliminar = document.createElement('button');
+                botonEliminar.innerText = "Eliminar";
+                botonEliminar.className = "delete";
+                botonEliminar.type = 'button';
+                botonEliminar.style.marginLeft = "7px";
+                botonEliminar.style.marginTop = "4px";
+                span.innerText =  user.name +`\n Tarea: ` + todo.title;
+
+                // console.log(`este ese res.title`, todo.title);
+                try {
+                    if(todo.completed) {
+                    li.style.textDecoration = 'line-through rgb(241, 136, 136)';
+                    const eliminar = event => {
+                    const eliminarToDo = event.target.parentElement;
+                    eliminarToDo.remove();
+                } 
+                    botonEliminar.addEventListener('click', eliminar);
+                } else {
+                    const mostrarAlerta = event => {
+                        event.target.parentElement;
+                        alert(`La tarea: ${todo.title} \nNo fue realizada`);         
+                    } 
+                    botonEliminar.addEventListener('click', mostrarAlerta);
+                } 
+            } catch (err) {
+                console.log(err);
+            }
+              
+                li.appendChild(span);
+                li.appendChild(botonEliminar);
+                document.querySelector('ul').appendChild(li);
+              
+            });          
+        })
+    })    
+}
+
+const load = () => {
+    const botonMostrar = document.querySelector('#bttMostrar');
+    botonMostrar.addEventListener('click', mostrarToDo);
+   
+}
